eb17a6ccfbf2447c44af343575f57cf84ede0ecd
Padmore Adusei Ening <padmore27@gmail.com>
2025-12-07 19:59:35 +0000

    Fix test method name for multiplication

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -14,13 +14,13 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertIsNone(self.calc.divide(5, 0))   
+        self.assertIsNone(self.calc.divide(5, 0))
 
 if __name__ == "__main__":
     unittest.main()
