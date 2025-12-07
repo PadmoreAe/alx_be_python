6ffbd6c347952e0f07e4069ca1109083724fd29c
Padmore Adusei Ening <padmore27@gmail.com>
2025-12-07 19:08:05 +0000

    Add BankAccount class and main-0.py for command line interactions

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,17 +1,15 @@
 class BankAccount:
-    def __init__(self, initial_balance=0.0):
-        self.balance = initial_balance
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
 
     def deposit(self, amount):
-        self.balance += amount
-        print(f"Deposited: ${amount:.1f}")
+        self.account_balance += amount
 
     def withdraw(self, amount):
-        if amount <= self.balance:
-            self.balance -= amount
-            print(f"Withdrew: ${amount:.1f}")
-        else:
-            print("Insufficient funds.")
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
 
     def display_balance(self):
         print(f"Current Balance: ${self.balance:.2f}")
