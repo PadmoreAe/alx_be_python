1f59b054f2e18fd8afee59a35bcd4164652ba7c2
Padmore Adusei Ening <padmore27@gmail.com>
2025-12-07 20:00:41 +0000

    Fix test method name for division

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -18,7 +18,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertIsNone(self.calc.divide(5, 0))
 
